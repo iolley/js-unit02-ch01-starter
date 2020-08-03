d5d3a314f062952185d4c63e02e8684b6c4618f9
iorisoeda <iolley814@gmail.com>
2020-08-03 13:49:13 +0200

    added shift and push into function addRandomNum

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import _ from 'lodash';
 
+let numbers = [];
+
 function addRandomNum() {
   const mainEl = document.getElementById('main');
   let p = document.createElement('p')
   p.innerHTML = "Random Number: " + _.random(0, 10).toString();
   document.body.appendChild(p)
+//.shift  先頭を消す　５個以上の場合
+//.push   後ろに入れる　５個以上になり、先頭が抜けた場合
+  if (numbers >= 5) {
+    numbers.shift();
+  } else {
+    numbers.push();
+  }
 }
-
 {
   const button1 = document.getElementById('button1')
   button1.addEventListener("click", addRandomNum);
   console.log("ready");
-  //push-test
+  //
 }
