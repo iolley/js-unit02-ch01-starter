bb939d5b585428a958175a478aeac52daed7dfd8
iorisoeda <iolley814@gmail.com>
2020-08-24 17:31:23 +0200

    function recordNum

--- src/index.js
+++ src/index.js
@@ -5,25 +5,24 @@ let numbers = [];
 function addRandomNum() {
   const mainEl = document.getElementById('main');
   let p = document.createElement('p')
-  p.innerHTML = "Random Number: " + _.random(0, 10).toString();
+  const randomNum = _.random(0, 10)
+  p.innerHTML = "Random Number: " + randomNum.toString();
   document.body.appendChild(p)
 //.shift  先頭を消す　５個以上の場合
 //.push   後ろに入れる　５個以上になり、先頭が抜けた場合
   if (numbers.length === 5) {
     console.log('ここはshift()')
-    numbers.shift(_.random(0, 10));
+    numbers.shift(randomNum);
   }
   console.log('ここはif文の外');
-  numbers.push(_.random(0, 10));
+  numbers.push(randomNum);
  
-}
-
+  function recordNum(){
+    randomNum++
+  }
 {
   const button1 = document.getElementById('button1')
   button1.addEventListener("click", addRandomNum);
-  console.log("ready");
-  
+  console.log("ready"); 
 }
-function recordNum(){
 }
-
