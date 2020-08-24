3c88527a52ba3c46e51db5904b931e35491ad0ca
iorisoeda <iolley814@gmail.com>
2020-08-24 20:46:00 +0200

    corrected global scope

--- src/index.js
+++ src/index.js
@@ -8,21 +8,22 @@ function addRandomNum() {
   const randomNum = _.random(0, 10)
   p.innerHTML = "Random Number: " + randomNum.toString();
   document.body.appendChild(p)
+  } 
 //.shift  先頭を消す　５個以上の場合
 //.push   後ろに入れる　５個以上になり、先頭が抜けた場合
   if (numbers.length === 5) {
-    console.log('ここはshift()')
+    console.log('ここはshift()');
     numbers.shift(randomNum);
+  
+    console.log('ここはif文の外');
+    numbers.push(randomNum);
   }
-  console.log('ここはif文の外');
-  numbers.push(randomNum);
- 
-  function recordNum(){
+  function randomNum() {
     randomNum++
   }
+
 {
   const button1 = document.getElementById('button1')
   button1.addEventListener("click", addRandomNum);
   console.log("ready"); 
-}
 }
