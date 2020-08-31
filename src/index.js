ba1eebb55507d734eaf79562a08ab1fa3900a990
iorisoeda <iolley814@gmail.com>
2020-08-31 12:59:26 +0200

    corrected

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import _ from 'lodash';
 
 let numbers = [];
 
+// 関数に後置インクリメントは使用できません。
+    // この範囲は関数randomNum関数自体の用意をしている宣言範囲内です。
+    // 準備中の関数内に準備中の関数自体を呼び出すことはできたかどうかを、基礎編Lesson 9とMDNの関数の項目ページを調べて確認しましょう
+function randomNum() {
+  if( numbers.length===5)
+  numbers++
+}
+
 function addRandomNum() {
   const mainEl = document.getElementById('main');
   let p = document.createElement('p')
@@ -14,13 +22,18 @@ function addRandomNum() {
   if (numbers.length === 5) {
     console.log('ここはshift()');
     numbers.shift(randomNum);
-  
+  }
+  // // ↓ この記述はif文内のスコープに書いてあったのかどうかを確認してください。
+    // 文字列で「ここはif文の外」と用意をしたことを思い出しましょう
+    // 以前のようにきちんとconsole.logで「ここはif文の外」や「ここはshift()」と出てこないのはこれが原因です
+    // なぜelseを省略できたかを思い出しましょう
     console.log('ここはif文の外');
     numbers.push(randomNum);
-  }
-  function randomNum() {
-    randomNum++
-  }
+  
+  // 関数randumNumはここに用意をしておいた方がいいかどうかはまだわかりません。
+  // 関数addRandomNum内で確実に呼び出すことがわからないからです
+  // どこで呼び出すかが未定の関数ば、基本的にグローバルスコープにまずは用意してください
+  // ここは関数addRandomNumのローカルスコープ内です
 
 {
   const button1 = document.getElementById('button1')
